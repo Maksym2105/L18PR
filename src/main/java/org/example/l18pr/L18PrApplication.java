c139c2a059bd9e99a54cc419af8e9d4f2116e2eb
Maksym2105 <maxdetrai02@gmail.com>
2025-04-04 15:21:57 +0300

    Init commit

---
+++ src/main/java/org/example/l18pr/L18PrApplication.java
@@ -0,0 +1,13 @@
+package org.example.l18pr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L18PrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(L18PrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/l18pr/L18PrApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.l18pr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L18PrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
